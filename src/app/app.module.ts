ad6305be77e23c9f06d1ac980201bc44e1034988
MEHELLEB Yacine <archange34@hotmail.fr>
2024-03-23 00:54:35 +0100

    création de trois composants

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { FormComponent } from './form/form.component';
 import { FormProductComponent } from './form-product/form-product.component';
+import { AddProductComponent } from './add-product/add-product.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FormComponent,
-    FormProductComponent
+    FormProductComponent,
+    AddProductComponent,
+    NotFoundComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
