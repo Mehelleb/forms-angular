abe8a4028ef88e59a96bfdfbc4b09ac63777c938
MEHELLEB Yacine <archange34@hotmail.fr>
2024-03-24 00:43:31 +0100

    création de aoo-routing.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,32 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 import { FormComponent } from './form/form.component';
 import { FormProductComponent } from './form-product/form-product.component';
-import { AddProductComponent } from './add-product/add-product.component';
-import { NotFoundComponent } from './not-found/not-found.component';
 import { ProductsComponent } from './products/products.component';
-import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const ROUTES: Routes=[
-  {path:'products', component: ProductsComponent},
-  {path:'form', component: FormComponent},
-  {path:'**', component: NotFoundComponent},
-];
+
 
 @NgModule({
   declarations: [
     AppComponent,
     FormComponent,
     FormProductComponent,
-    AddProductComponent,
-    NotFoundComponent,
-    ProductsComponent
+    ProductsComponent,
+    HomeComponent
+
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(ROUTES)
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/model/Product';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent {
-
+export class ProductsComponent implements OnInit{
+  product!:Product[]
+ngOnInit(): void {
+  this.product=[
+    {id:1, title:"T-shirt-1", quantity:3, like:2},
+    {id:2, title:"T-shirt-2", quantity:3, like:2},
+    {id:3, title:"T-shirt-3", quantity:3, like:2}
+  ];
+}
 }
